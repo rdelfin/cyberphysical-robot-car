c44046137b8312356d96da31aed5e8ab1b09b7d8
Eitan-Yarmush <eyarmush212@gmail.com>
2016-05-06 14:58:37 -0500

    Fixed wall follower

--- race/race/src/wall_follower.py
+++ race/race/src/wall_follower.py
@@ -8,22 +8,32 @@ from race.msg import wall_dist  #x, distance; theta, radians
 
 desired_trajectory = 0.6
 vel = 9
+turnMode = False
 
 pub = rospy.Publisher('error', pid_input, queue_size=10)
 
 
 
 def callback(data):
+    global turnMode
     rospy.wait_for_service('corner')
     try:
         corner_service = rospy.ServiceProxy('corner', corner_loc)
         corner_data = corner_service()
     except rospy.ServiceException, e:
         print "service call failed: " + str(e)
+    
+    if(turnMode):
+        error = 1
+        
+        if(not (corner_data.found)):
+            turnMode = False
+    else:
+        error = data.distance - desired_trajectory + data.theta
+        
+        if(corner_data.found and corner_data.y < 0.3):
+            turnMode = True
 
-
-
-    error = data.distance - desired_trajectory + data.theta
 
 
     msg = pid_input()
